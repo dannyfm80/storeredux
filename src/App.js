32d047cfc6778e4b5fbfd3d7025ecd0302c3c92e
Sergio Danny Flores Montero <dannyfm80@gmail.com>
2021-03-31 15:09:27 -0400

    uesSelector to access state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={ProductListing} />
+          <Route exact path="/product/:productId" component={ProductDetail} />
+          <Route>404 Not Found!!</Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
